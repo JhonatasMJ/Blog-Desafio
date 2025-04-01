9285e958c61e9c4be665cee7d188b6f8cda26dd6
JhonatasMJ <jhonatasjhmj@gmail.com>
2025-04-01 12:49:41 -0300

    Env firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,22 +3,18 @@ import { getAuth } from "firebase/auth"
 import { getDatabase } from "firebase/database"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnSFD56Go7yUZFMIvgeISQblLIY-FJ1-w",
-  authDomain: "blog-ecaae.firebaseapp.com",
-  projectId: "blog-ecaae",
-  storageBucket: "blog-ecaae.firebasestorage.app",
-  messagingSenderId: "542566894565",
-  appId: "1:542566894565:web:ed41cf70675bb1a4223d0c",
-  measurementId: "G-0PR8CP5LCJ",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
-
 const app = initializeApp(firebaseConfig)
-
 
 export const auth = getAuth(app)
 export const database = getDatabase(app)
 
-
 export default app
-
